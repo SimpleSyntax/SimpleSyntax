f45bdea1077af8cd6ecd2813f2ebda0dcc4424db
SimpleSyntax <124462121+SimpleSyntax@users.noreply.github.com>
2023-02-17 13:15:31 +1100

    Datatypes python file for SimpleSyntax

---
+++ datatypes.py
@@ -0,0 +1,19 @@
+# this is an int data type
+int_num = 10
+
+# this is a float data type
+float_num = 10.365
+
+# this is a string data type
+string_word = "this is a string"
+
+# this is a list data type
+list_words = ['car','dog','cat','computer',1]
+
+# these are boolean data types
+boolean_type_1 = True
+boolean_type_2 = False
+boolean_type_3 = 5
+
+print(int_num,"\n",float_num,"\n",string_word,"\n",list_words,"\n",boolean_type_1
+	,"\n",boolean_type_2,"\n",boolean_type_3)
